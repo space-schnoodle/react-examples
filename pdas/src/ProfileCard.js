2a6c1a2e9ff62242bd7bdd0de2c6642873fe22b6
space-schnoodle <space.schnoodle@gmail.com>
2023-01-15 10:04:37 -0500

    finish pdas project and create animals project

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,8 +1,8 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
   return (
     <div className="card">
       <div className="card-image">
-        <figure className="mage is-1by1">
+        <figure className="image is-1by1">
           <img src={image} alt="pda logo" />
         </figure>
       </div>
@@ -11,6 +11,7 @@ function ProfileCard({ title, handle, image }) {
           <p className="title is-4">{title}</p>
           <p className="subtitle is-6">{handle}</p>
         </div>
+        <div className="content">{description}</div>
       </div>
     </div>
   );
