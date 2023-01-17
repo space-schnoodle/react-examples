4406aef51223cc23497ec764db30abe73570c70d
space-schnoodle <space.schnoodle@gmail.com>
2023-01-17 14:38:48 -0500

    update api

--- pics/src/api.js
+++ pics/src/api.js
@@ -3,6 +3,7 @@ import axios from "axios";
 const searchImages = async (term) => {
   const response = await axios.get("https://api.unsplash.com/search/photos", {
     headers: {
+      // ADD YOUR KEY FROM UNSPLASH
       Authorization: "Client-ID KfcT7f3Lq7LN175B0pX1Tfb_5pT0m-fOKwGBaE4XqE0",
     },
     params: {
