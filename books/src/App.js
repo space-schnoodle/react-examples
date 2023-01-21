663546ffdfaaa8f364b25005d4ef4de988fdc505
space-schnoodle <space.schnoodle@gmail.com>
2023-01-21 09:59:17 -0500

    added add and delete books

--- books/src/App.js
+++ books/src/App.js
@@ -1,15 +1,31 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
-    console.log("need to add book of title", title);
+    const updatedBooks = [
+      ...books,
+      {
+        id: Math.round(Math.random() * 9999),
+        title,
+      },
+    ];
+    setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app">
+      <BookList books={books} onDelete={deleteBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- books/src/components/BookList.js
+++ books/src/components/BookList.js
@@ -1,5 +1,11 @@
-function BookList() {
-  return <div>Book List</div>;
+import BookShow from "./BookShow";
+
+function BookList({ books, onDelete }) {
+  const renderedBooks = books.map((book) => {
+    return <BookShow onDelete={onDelete} key={book.id} book={book} />;
+  });
+
+  return <div className="book-list">{renderedBooks}</div>;
 }
 
 export default BookList;
