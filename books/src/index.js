3bbdcdb772c886b5ac560eee6a2e93f993429441
space-schnoodle <space.schnoodle@gmail.com>
2023-02-08 17:34:59 -0300

    Fixing commit email

--- books/src/index.js
+++ books/src/index.js
@@ -8,7 +8,7 @@ const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 root.render(
-<BooksContext.Provider value={5}>
+<BooksContext.Provider value={10}>
     <App />
 </BooksContext.Provider>
 );
